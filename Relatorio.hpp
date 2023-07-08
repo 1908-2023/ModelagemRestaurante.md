f850c596f30ea282165c920050095cc0f5a9b9af
VitorMaia12 <138622751+VitorMaia12@users.noreply.github.com>
2023-07-07 23:13:46 -0300

    Relatorio.hpp

--- Relatorio.hpp
+++ Relatorio.hpp
@@ -1,66 +1,24 @@
-#include "Relatorio.hpp"
-#include <iostream>
+#ifndef RELATORIO_HPP
+#define RELATORIO_HPP
 
-Relatorio::Relatorio()
-{
-    custoTotal = 0.0;
-    lucroTotal = 0.0;
-    despesaTotal = 0.0;
-}
+#include <vector>
+#include "Pedido.hpp"
 
-void Relatorio::adicionarPedido(const Pedido &pedido)
+class Relatorio
 {
-    pedidos.push_back(pedido);
-}
-/*
-void Relatorio::calcularCustos()
-{
-    custoTotal = 0.0;
-    for (const auto &pedido : pedidos)
-    {
-        custoTotal += pedido; //.calcularCustoProducao();
-    }
-}
-*/
-double Relatorio::calcularLucro() const
-{
-    return lucroTotal - calcularDespesa();
-}
+private:
+    std::vector<Pedido> pedidos;
+    double custoTotal;
+    double lucroTotal;
+    double despesaTotal;
 
-double Relatorio::calcularDespesa() const
-{
-    // Lógica para calcular as despesas do restaurante
-    double despesa = 0.0;
-    // Exemplo de despesa fixa mensal
-    double despesaFixaMensal = 2000.0;
-    // Exemplo de despesa variável por pedido
-    double despesaVariavelPorPedido = 2.5;
+public:
+    Relatorio();
+    void adicionarPedido(const Pedido &pedido);
+    void calcularCustos();
+    double calcularLucro() const;
+    double calcularDespesa() const;
+    void mostrarRelatorio() const;
+};
 
-    // Calcula a despesa total com base no número de pedidos
-    despesa = despesaFixaMensal + (despesaVariavelPorPedido * pedidos.size());
-
-    return despesa;
-}
-
-void Relatorio::mostrarRelatorio() const
-{
-   // calcularCustos();
-    std::cout << "Relatório de vendas:\n";
-    std::cout << "-------------------------------------------\n";
-    for (const auto &pedido : pedidos)
-    {
-      //  std::cout << "ID do Pedido: " << pedido.getId() << "\n";
-        std::cout << "Cliente: " << pedido.getCliente() << "\n";
-    //    std::cout << "Itens do Pedido:\n";
-     //   for (const auto &item : pedido.getItens())
-       // {
-      //      std::cout << "- " << item.getNome() << " (" << item.getQuantidade() << "x)\n";
-      //  }
-      //  std::cout << "-------------------------------------------\n";
-   // }
-    std::cout << "Custo Total: R$" << custoTotal<<"\n";
-    std::cout << "Lucro Total: R$" << lucroTotal<<"\n";
-    std::cout << "Despesa Total: R$" << despesaTotal<<"\n";
-    std::cout << "Lucro Liquido: R$" << calcularLucro()<<"\n";
-}
-}
+#endif
