3cdf46961b07f2ac84b64b1d00bca5df583eb22b
VitorMaia12 <138622751+VitorMaia12@users.noreply.github.com>
2023-07-07 23:20:01 -0300

    Cardapio.cpp

--- Cardapio.cpp
+++ Cardapio.cpp
@@ -3,7 +3,8 @@
 
 Cardapio::Cardapio() {}
 
-void Cardapio::adicionarItem(const std::string& nome, const std::string& descricao, double preco) {
+void Cardapio::adicionarItem(const std::string &nome, const std::string &descricao, double preco)
+{
     Item item;
     item.nome = nome;
     item.descricao = descricao;
@@ -11,19 +12,24 @@ void Cardapio::adicionarItem(const std::string& nome, const std::string& descric
     itens.push_back(item);
 }
 
-void Cardapio::removerItem(const std::string& nome) {
-    for (auto it = itens.begin(); it != itens.end(); ++it) {
-        if (it->nome == nome) {
+void Cardapio::removerItem(const std::string &nome)
+{
+    for (auto it = itens.begin(); it != itens.end(); ++it)
+    {
+        if (it->nome == nome)
+        {
             itens.erase(it);
             break;
         }
     }
 }
 
-void Cardapio::exibirCardapio() const {
+void Cardapio::exibirCardapio() const
+{
     std::cout << "Cardápio do Restaurante:\n";
     std::cout << "-------------------------------------------\n";
-    for (const auto& item : itens) {
+    for (const auto &item : itens)
+    {
         std::cout << "Nome: " << item.nome << "\n";
         std::cout << "Descrição: " << item.descricao << "\n";
         std::cout << "Preço: R$" << item.preco << "\n\n";
